37800eb79fe5eb23da8f6e8c0d00c92000fb5dca
Nice <hoangkimc93@gmail.com>
2024-01-24 17:24:39 +0700

    refactor(login-menu): remove reloadDocument

--- src/components/RightContent/Login.tsx
+++ src/components/RightContent/Login.tsx
@@ -7,7 +7,6 @@ const Login: React.FC = () => {
   return (
     <Link
       to={PATH_LOGIN}
-      reloadDocument
       className="px-2 flex justify-center align-center w-fit relative menu-item "
     >
       <div className="flex justify-center items-center text-neutral-1 hover:text-neutral-1 px-2 ">
