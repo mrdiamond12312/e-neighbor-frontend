4e974df3a2980205d5b95b928b8cf3099dc41ec3
Phú Nguyễn <trongphu012345@gmail.com>
2024-04-20 04:13:18 +0700

    fix: Changed form react hook, not updated UI and image dragger for citizen identification

--- src/pages/User/Profile/components/FormInfo/index.tsx
+++ src/pages/User/Profile/components/FormInfo/index.tsx
@@ -1,28 +1,24 @@
-import {
-  Button,
-  Input,
-  DatePicker,
-  Form,
-  Popconfirm,
-  type FormProps,
-  type PopconfirmProps,
-} from 'antd';
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { Button, Form, FormProps, Popconfirm, type PopconfirmProps } from 'antd';
+import ImgCrop from 'antd-img-crop';
 import moment from 'moment';
-import { useEffect, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useIntl } from 'umi';
 
+import InputText from '@/components/Input';
+import DatePicker from '@/components/Input/DatePicker';
+import HiddenInput from '@/components/Input/HiddenInput';
+import { ImageDragger } from '@/components/Input/ImageDragger';
 import useEditForm from '@/pages/user/profile/hook/useEditForm';
 
-dayjs.extend(customParseFormat);
 const dateFormat = 'YYYY/MM/DD';
+const { Item } = Form;
 
 const FormInfo: React.FC = () => {
+  const [form] = Form.useForm();
   const { formatMessage } = useIntl();
 
-  const [dataProfileAPI, setDataProfileAPI] = useState<any>();
+  const [, setDataProfileAPI] = useState<any>();
 
   const { handleEditMode } = useEditForm();
 
@@ -35,11 +31,6 @@ const FormInfo: React.FC = () => {
     setDataProfileAPI(dataTemp);
     handleEditMode(false);
   };
-  // console.log(dataProfileAPI)
-
-  // const onFinishFailed: FormProps<API.TAuthProfile>['onFinishFailed'] = (errorInfo) => {
-  //   console.log('Failed:', errorInfo);
-  // };
 
   const confirm: PopconfirmProps['onConfirm'] = () => {
     handleEditMode(true);
@@ -49,206 +40,170 @@ const FormInfo: React.FC = () => {
     handleEditMode(false);
   };
 
-  const { register, handleSubmit, control, setValue } = useForm();
-
-  useEffect(() => {
-    register('userName');
-    register('password');
-    register('email');
-    register('fullName');
-    register('address');
-    register('detailedAddress');
-    register('phoneNumber');
-  }, [register]);
-
-  const getChangeHandlerWithEvent = (name: string) => (event: { target: { value: any } }) =>
-    setValue(name, event.target.value);
+  const { handleSubmit, control } = useForm();
 
   return (
     <Form
+      form={form}
       name="basic"
-      labelCol={{ span: 6 }}
-      wrapperCol={{ span: 18 }}
-      style={{ maxWidth: 700 }}
-      initialValues={{
-        userName: dataProfileAPI?.userName,
-        password: dataProfileAPI?.password,
-        fullName: dataProfileAPI?.fullName,
-        phoneNumber: dataProfileAPI?.phoneNumber,
-        email: dataProfileAPI?.email,
-        address: dataProfileAPI?.address,
-        detailedAddress: dataProfileAPI?.detailedAddress,
-        dob: dayjs(dataProfileAPI?.dob) || dayjs(),
-      }}
+      wrapperCol={{ span: 20 }}
+      //   initialValues={{
+      //     userName: dataProfileAPI?.userName,
+      //     password: dataProfileAPI?.password,
+      //     fullName: dataProfileAPI?.fullName,
+      //     phoneNumber: dataProfileAPI?.phoneNumber,
+      //     email: dataProfileAPI?.email,
+      //     address: dataProfileAPI?.address,
+      //     detailedAddress: dataProfileAPI?.detailedAddress,
+      //     dob: dayjs(dataProfileAPI?.dob) || dayjs(),
+      //   }}
       onFinish={handleSubmit(onFinish)}
       autoComplete="off"
     >
-      <div className="font-semibold mt-3">
+      <Item className="font-semibold mt-3">
         {formatMessage({
           id: 'user.profile.input.login',
           defaultMessage: 'Login Information',
         })}
-      </div>
-      <Form.Item<API.TAuthProfile>
+      </Item>
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.username',
           defaultMessage: 'Username',
         })}
-        name="userName"
-        rules={[
-          {
-            required: true,
-            message: formatMessage({
-              id: 'user.profile.input.username.required',
-              defaultMessage: 'Please enter a username',
-            }),
-          },
-        ]}
+        required
       >
-        <Input onChange={getChangeHandlerWithEvent('userName')} />
-      </Form.Item>
-      <Form.Item<API.TAuthProfile>
+        <InputText name="username" control={control} />
+      </Item>
+
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.password',
           defaultMessage: 'Password',
         })}
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: formatMessage({
-              id: 'user.profile.input.password.required',
-              defaultMessage: 'Please enter a password',
-            }),
-          },
-        ]}
+        required
       >
-        <Input.Password onChange={getChangeHandlerWithEvent('password')} />
-      </Form.Item>
-      <div className="font-semibold">
+        <HiddenInput placeholder="******" control={control} name="password" />
+      </Item>
+
+      <Item className="font-semibold">
         {formatMessage({
           id: 'user.profile.input.contact',
           defaultMessage: 'Contact Information',
         })}
-      </div>
-      <Form.Item<API.TAuthProfile>
+      </Item>
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.email',
           defaultMessage: 'Email',
         })}
-        name="email"
-        rules={[
-          {
-            type: 'email',
-            message: formatMessage({
-              id: 'user.profile.input.invalidEmail',
-              defaultMessage: 'Your email address is invalid',
-            }),
-          },
-          {
-            required: true,
-            message: formatMessage({
-              id: 'user.profile.input.enterEmail',
-              defaultMessage: 'Please enter your email address',
-            }),
-          },
-        ]}
+        required
       >
-        <Input onChange={getChangeHandlerWithEvent('email')} />
-      </Form.Item>
-      <Form.Item<API.TAuthProfile>
+        <InputText name="email" control={control} />
+      </Item>
+
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.fullName',
           defaultMessage: 'Full Name',
         })}
-        name="fullName"
       >
-        <Input onChange={getChangeHandlerWithEvent('fullName')} />
-      </Form.Item>
-      <Form.Item<API.TAuthProfile>
+        <InputText name="fullName" control={control} />
+      </Item>
+
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.address',
           defaultMessage: 'Address',
         })}
-        name="address"
       >
-        <Input onChange={getChangeHandlerWithEvent('address')} />
-      </Form.Item>
-      <Form.Item<API.TAuthProfile>
+        <InputText name="address" control={control} />
+      </Item>
+
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.detailAddress',
           defaultMessage: 'Detail Address',
         })}
-        name="detailedAddress"
       >
-        <Input onChange={getChangeHandlerWithEvent('detailedAddress')} />
-      </Form.Item>
-      <Form.Item<API.TAuthProfile>
+        <InputText name="detailAddress" control={control} />
+      </Item>
+
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.dateOfBirth',
           defaultMessage: 'Date of Birth',
         })}
-        name="dob"
       >
-        <Controller
-          name="dob"
-          control={control}
-          render={({ field }) => <DatePicker {...field} format={dateFormat} />}
-        />
-      </Form.Item>
-      <Form.Item<API.TAuthProfile>
+        <DatePicker format="DD-MM-YYYY" name="dob" control={control} />
+      </Item>
+
+      <Item
+        className="flex items-center space-x-4 mt-2"
         label={formatMessage({
           id: 'user.profile.input.phoneNumber',
           defaultMessage: 'Phone Number',
         })}
-        name="phoneNumber"
       >
-        <Input onChange={getChangeHandlerWithEvent('phoneNumber')} />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 0, span: 24 }}>
-        <div className="flex justify-end">
-          <Button
-            type="primary"
-            style={{ marginRight: '5px' }}
-            className="btn-medium btn-primary"
-            htmlType="submit"
-          >
+        <InputText name="phoneNumber" control={control} />
+      </Item>
+
+      <Item
+        className="flex items-center space-x-4 mt-2"
+        label={formatMessage({
+          id: 'user.profile.input.citizenCardNumber',
+          defaultMessage: 'Citizen Card Number',
+        })}
+      >
+        <ImgCrop aspect={1} rotationSlider>
+          <ImageDragger control={control} maxCount={2} className="custom-ant-upload" name="CCCD" />
+        </ImgCrop>
+      </Item>
+
+      <div className="flex items-center space-x-4 mt-2">
+        <Button type="primary" className="btn-medium btn-primary" htmlType="submit">
+          {formatMessage({
+            id: 'user.profile.input.saveChangesBtn',
+            defaultMessage: 'Save Changes',
+          })}
+        </Button>
+        <Popconfirm
+          title={formatMessage({
+            id: 'user.profile.input.confirmExit',
+            defaultMessage: 'Confirm Exit',
+          })}
+          description={formatMessage({
+            id: 'user.profile.input.cancel',
+            defaultMessage: 'If you exit, you will lose your modified data.',
+          })}
+          onConfirm={confirm}
+          onCancel={cancel}
+          okText={formatMessage({
+            id: 'user.profile.input.yes',
+            defaultMessage: 'Yes',
+          })}
+          cancelText={formatMessage({
+            id: 'user.profile.input.no',
+            defaultMessage: 'No',
+          })}
+          okButtonProps={{ className: 'btn-medium btn-primary' }}
+          cancelButtonProps={{ className: 'btn-medium btn-default' }}
+        >
+          <Button className="btn-medium btn-default">
             {formatMessage({
-              id: 'user.profile.input.saveChangesBtn',
-              defaultMessage: 'Save Changes',
+              id: 'user.profile.input.cancelBtn',
+              defaultMessage: 'Cancel',
             })}
           </Button>
-          <Popconfirm
-            title={formatMessage({
-              id: 'user.profile.input.confirmExit',
-              defaultMessage: 'Confirm Exit',
-            })}
-            description={formatMessage({
-              id: 'user.profile.input.cancel',
-              defaultMessage: 'If you exit, you will lose your modified data.',
-            })}
-            onConfirm={confirm}
-            onCancel={cancel}
-            okText={formatMessage({
-              id: 'user.profile.input.yes',
-              defaultMessage: 'Yes',
-            })}
-            cancelText={formatMessage({
-              id: 'user.profile.input.no',
-              defaultMessage: 'No',
-            })}
-            okButtonProps={{ className: 'btn-medium btn-primary' }}
-            cancelButtonProps={{ className: 'btn-medium btn-default' }}
-          >
-            <Button className="btn-medium btn-default">
-              {formatMessage({
-                id: 'user.profile.input.cancelBtn',
-                defaultMessage: 'Cancel',
-              })}
-            </Button>
-          </Popconfirm>
-        </div>
-      </Form.Item>
+        </Popconfirm>
+      </div>
     </Form>
   );
 };
